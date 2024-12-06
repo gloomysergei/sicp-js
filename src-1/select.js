e7704bee3f6f46afb35a65c1de4330a89d29cd61
gloomysergei <almazopal@gmail.com>
2024-12-06 20:15:11 +0300

    creation: src-1; delete: half.test.js

---
+++ src-1/select.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+import {
+  /* eslint-disable */
+  l,
+  cons as consList,
+  isList,
+  isEmpty,
+  head,
+  tail,
+  concat,
+  toString as listToString,
+} from "@hexlet/pairs-data";
+
+import {
+  is,
+  hasChildren,
+  children,
+  filter,
+  reduce,
+  toString as htmlToString,
+  getValue,
+} from "@hexlet/html-tags"; /* eslint-enable */
