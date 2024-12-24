f62476d1eaceddd293e528893df7c9826676759d
gloomysergei <almazopal@gmail.com>
2024-12-24 03:24:10 +0300

    project: solving step-1

--- project/game.js
+++ project/game.js
@@ -14,16 +14,28 @@ const run = (player1, player2, cards) => {
   const iter = (health1, name1, health2, name2, order, log) => {
     // BEGIN (write your solution here)
     if (health1 <= 0) {
-      return cons(`${name1} был убит`, log);
-    } else {
-      const card = random(cards); // получили карту
-      const cardName = car(card); // наименование карты
-      const damage = cdr(card); // наносимый урон
-      const message = `Игрок '${name1}' применил '${cardName}'
-        против '${name2}' и нанес урон '${damage}'`;
-      health1 - damage;
-      return cons();
+      const element = cons(car(head(log)), `${name1} был убит`); // очень хитро
+      const result = consList(element, log);
+      console.log(listToString(result));
+      return result;
     }
+    const card = random(cards); // получили карту
+    const cardName = car(card); // наименование карты
+    const damage = cdr(card)(); // наносимый урон
+    const message = `Игрок '${name1}' применил '${cardName}'
+        против '${name2}' и нанес урон '${damage}'`;
+    const newHealth = health2 - damage;
+    let stats;
+    if (order === 1) {
+      stats = cons(cons(health1, newHealth), message);
+    } else if (order === 2) {
+      stats = cons(cons(newHealth, health1), message);
+    }
+    // Формируем элемент лога формата cons(cons(health1, health2), message)
+    // и добавляем его в лог.
+
+    const newLog = consList(stats, log);
+    return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, newLog);
     // END
   };
 
