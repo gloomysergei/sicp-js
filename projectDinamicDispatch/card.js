c69dc1b269ef74c42598bb69834e011e5c31333f
gloomysergei <almazopal@gmail.com>
2025-02-05 01:24:26 +0300

    change: projectDinamicDispatch/card.js

--- projectDinamicDispatch/card.js
+++ projectDinamicDispatch/card.js
@@ -1,3 +1,5 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable import/prefer-default-export */
 /* eslint-disable quotes */
 // @ts-check
 
@@ -5,9 +7,10 @@ import { contents } from "@hexlet/tagged-types";
 import { getMethod } from "./generic.js";
 
 // @ts-ignore
-const getName = (self) => getMethod(self, "getName")(contents(self));
-export default getName;
+export const getName = (self) => getMethod(self, "getName")(contents(self));
 
 // BEGIN (write your solution here)
-
+export const damage = (self, health) =>
+  // @ts-ignore
+  getMethod(self, "damage")(contents(self), health);
 // END
